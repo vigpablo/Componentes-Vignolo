547ebaefc44044f07736b19e6cb5446cdbb35092
Pablo Vignolo <vignolopbl@gmail.com>
2023-03-22 11:52:06 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    MainComponent,
+    PageWrapperComponent,
   ],
   imports: [
     BrowserModule,
